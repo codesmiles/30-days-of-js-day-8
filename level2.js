d7f48a3d47219b3912181b54018a8282f30c1193
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-03-20 16:48:56 +0100

    Update

--- level2.js
+++ level2.js
@@ -72,18 +72,18 @@ const users = {
   },
 };
 const findPersonWithHighestSkills = () => {
-//   Gives me the necessary object keys as an array
-    let userKeys = Object.keys(users);
-    
-    //all the arr length of all the skills
-    const pushedArrLength = [];
+  //   Gives me the necessary object keys as an array
+  let userKeys = Object.keys(users);
 
-    for (i = 0; i < userKeys.length; i++) {  
+  //all the arr length of all the skills
+  const pushedArrLength = [];
+
+  for (i = 0; i < userKeys.length; i++) {
     pushedArrLength.push(users[userKeys[i]].skills.length);
-    };
-//   max array length
-    let maxSkills = Math.max(...pushedArrLength);
-    
+  }
+  //   max array length
+  let maxSkills = Math.max(...pushedArrLength);
+
   for (i = 0; i < userKeys.length; i++) {
     if (users[userKeys[i]].skills.length === maxSkills) {
       return userKeys[i];
@@ -92,65 +92,66 @@ const findPersonWithHighestSkills = () => {
 };
 console.log(findPersonWithHighestSkills());
 
+// second method
+const findPersonWithHighestSkillSecond = () => {
+  let userKeys = Object.keys(users);
+  let maxSkills = 0;
+  let person;
+  for (i = 0; i < userKeys.length; i++) {
+    if (users[userKeys[i]].skills.length > maxSkills) {
+      maxSkills = users[userKeys[i]].skills.length;
+      person = userKeys[i];
+    }
+  }
+  return person;
+};
+
 // Count logged in users,count users having greater than equal to 50 points from the following object.
 
-// const users = {
-//   Alex: {
-//     email: 'alex@alex.com',
-//     skills: ['HTML', 'CSS', 'JavaScript'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 30
-//   },
-//   Asab: {
-//     email: 'asab@asab.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-//     age: 25,
-//     isLoggedIn: false,
-//     points: 50
-//   },
-//   Brook: {
-//     email: 'daniel@daniel.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-//     age: 30,
-//     isLoggedIn: true,
-//     points: 50
-//   },
-//   Daniel: {
-//     email: 'daniel@alex.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   },
-//   John: {
-//     email: 'john@john.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-//     age: 20,
-//     isLoggedIn: true,
-//     points: 50
-//   },
-//   Thomas: {
-//     email: 'thomas@thomas.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   },
-//   Paul: {
-//     email: 'paul@paul.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   }
-// }
+const value = Object.values(users);
+for (let i = 0; i < value.length; i++) {
+  if (value[i].points >= 50 && value[i].isLoggedIn === true) {
+    console.log(value[i]);
+  }
+}
+
 // Find people who are MERN stack developer from the users object
 
-// Set your name in the users object without modifying the original users object
+const MERNstackDeveloper = () => {
+  let userKeys = Object.keys(users);
+  for (i = 0; i < userKeys.length; i++) {
+    if (
+      users[userKeys[i]].skills.includes("MongoDB") &&
+      users[userKeys[i]].skills.includes("Express") &&
+      users[userKeys[i]].skills.includes("React") &&
+      users[userKeys[i]].skills.includes("Node")
+    ) {
+      console.log(users[userKeys[i]]);
+    }
+  }
+};
 
+// Set your name in the users object without modifying the original users object
+users.mike = {
+    email: "mikeflows@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 29,
+    isLoggedIn: true,
+    points: 50,
+  
+};
 // Get all keys or properties of users object
+const getAllKeys = () => {
+  let userKeys = Object.keys(users);
+  console.log(userKeys);
+
+};
 
 // Get all the values of users object
+const getAllValues = () => {
+  let userValues = Object.values(users);
+  console.log(userValues);
+}
 
 // Use the countries object to print a country name, capital, populations and languages.
+
