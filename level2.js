b4e4c5a8203e8319c28a3246b24bfec2a524e7da
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-03-20 03:03:36 +0100

    Breakthrough

--- level2.js
+++ level2.js
@@ -1,9 +1,96 @@
 // Find the person who has many skills in the users object.
-let users = {
+// if (users.keys) {
 
-}
-  
+// }
 
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+const findPersonWithHighestSkills = () => {
+//   Gives me the necessary object keys as an array
+    let userKeys = Object.keys(users);
+    
+    //all the arr length of all the skills
+    const pushedArrLength = [];
+
+    for (i = 0; i < userKeys.length; i++) {  
+    pushedArrLength.push(users[userKeys[i]].skills.length);
+    };
+//   max array length
+    let maxSkills = Math.max(...pushedArrLength);
+    
+  for (i = 0; i < userKeys.length; i++) {
+    if (users[userKeys[i]].skills.length === maxSkills) {
+      return userKeys[i];
+    }
+  }
+};
+console.log(findPersonWithHighestSkills());
 
 // Count logged in users,count users having greater than equal to 50 points from the following object.
 
@@ -57,7 +144,7 @@ let users = {
 //     isLoggedIn: false,
 //     points: 40
 //   }
-// }```
+// }
 // Find people who are MERN stack developer from the users object
 
 // Set your name in the users object without modifying the original users object
